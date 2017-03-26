11b8011014d309c80e52562ae8e3eb47a0936459
elijah <elijah@riseup.net>
2017-03-26 16:57:17 -0700

    don't pre-render sass partials

--- lib/amber/render/asset.rb
+++ lib/amber/render/asset.rb
@@ -26,10 +26,12 @@ module Amber
         src_ext = File.extname(src_file)
         renderer = RENDER_MAP[src_ext]
         if renderer
-          content = self.send(renderer[:method], *([src_file] + renderer[:args]))
-          new_dst_file = dst_file.sub(/#{Regexp.escape(src_ext)}$/, renderer[:new_suffix])
-          File.open(new_dst_file,'w') do |w|
-            w.write(content)
+          if File.basename(src_file) !~ /^_/  # skip partials
+            content = self.send(renderer[:method], *([src_file] + renderer[:args]))
+            new_dst_file = dst_file.sub(/#{Regexp.escape(src_ext)}$/, renderer[:new_suffix])
+            File.open(new_dst_file,'w') do |w|
+              w.write(content)
+            end
           end
         elsif File.basename(src_file) == '.htaccess'
           # existing htaccess file must be copied, not linked, since
